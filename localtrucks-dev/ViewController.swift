3850da9b1c37ee119fbf93a04356f4d2bc8c5ae4
Tim Wheeler <tim@timwheeler.com>
2019-02-09 23:35:58 -0500

    Initial Commit

---
+++ localtrucks-dev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  localtrucks-dev
+//
+//  Created by Timothy Wheeler on 2/9/19.
+//  Copyright © 2019 TimWheeler.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
